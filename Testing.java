ee6cc2122c1f40b6d60d06155acba64b450309f8
MGEDELA <mahendragedela22@gmail.com>
2025-10-04 12:53:25 +0530

    added1

--- Testing.java
+++ Testing.java
@@ -1,10 +1,11 @@
 public class Testing {
 public static void main(String[] args){
     System.out.println("This is my first program");
-    
-        String name = "Yuvraj is  andagadu";
 
+        String name = "Yuvraj is  andagadu";
         System.out.println(name);
+        System.out.println("sgv");
+        System.out.println("MAHENDRA");
 
 }
 }
